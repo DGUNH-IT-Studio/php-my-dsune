f1fce1d21fcb47f5d8d74517b34ea3dfb252e0e6
zhigibig <gamzat0004@mail.ru>
2024-01-25 16:44:14 +0300

    Make CRUD for Department entity.

---
+++ src/Form/DepartmentType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Department;
+use App\Entity\Faculty;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DepartmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('department_name')
+            ->add('faculty', EntityType::class, [
+                'class' => Faculty::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Department::class,
+        ]);
+    }
+}
